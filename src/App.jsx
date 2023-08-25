e40541e153e040bc78b28db3f526848779bde502
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-08-25 13:32:51 +0330

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import Accordion from "./components/Accordion/Accordion";
 import "./App.css"
 import { useState } from "react";
 const App = () => {
-  const [state,setState] = useState(false)
   return ( 
     <div className="wrapper">
       <Accordion/>
-      <p onClick={()=>setState(is=>!is)} className={`text ${state ? "open" : ""}`}>hello</p>
     </div>
    );
 }
