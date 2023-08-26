cb3430aeb1fa0609696b4718fc029cfef061275b
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-08-26 21:32:16 +0330

    accordion v2 developed,only one accordion at a time can be open and clicking on others will cause the current active accordion to close,this approach was developed via changing the value of our state from boolean to a number being our clicked on accordion id,and lifting up the state

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AccordionItem from "./AccordionItem";
 
 const data = [
@@ -5,7 +6,7 @@ const data = [
     id: 1,
     title: "Accordion 1",
     content:
-      "this is a sample test to test out the functionality of sample accordion",
+      "this is a hjvWh/lkfhskdfhsldKfh/slkDFHSlKFHSLKHFLSHDFLKHSDFvjhvhvvjhvjhvjvhjvhjvjhvjfhnfzgjsfhktdksryksksrkyryjzrdyjkkszrksxt7kxtuko8yldcyi;dcy9;vjhvjhvjvjhvjv test to test out the functionality of sample accordiSDGASDGSDGSDGSDGSDGSDGSDGSDGSDGon",
   },
   {
     id: 2,
@@ -18,14 +19,29 @@ const data = [
     title: "Accordion 3",
     content:
       "this is a sample test to test out the functionality of sample accordion",
-  }
+  },
 ];
-function Accordion(){
-  return <div className="Accordion">
-    {data.map((item)=>{
-        return <AccordionItem key={item.id} title={item.title} content={item.content}/>
-    })}
-  </div>;
-};
+function Accordion() {
+  const [open, setOpen] = useState(null);
+  const handleOpener = (id) => {
+    setOpen(id === open ? null : id);
+  };
+  return (
+    <div className="Accordion">
+      {data.map((item) => {
+        return (
+          <AccordionItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            content={item.content}
+            open={open}
+            setOpen={handleOpener}
+          />
+        );
+      })}
+    </div>
+  );
+}
 
 export default Accordion;
--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import {ChevronDownIcon,ChevronUpIcon} from "@heroicons/react/24/outline"
-function AccordionItem({ title, content }){
-    const [isOpen,setIsOpen] = useState(false)
+function AccordionItem({id, title, content ,open,setOpen}){
+    // const [isOpen,setIsOpen] = useState(false)
+    const isOpen = id === open
   return (
-    <div className={`accordion-item ${isOpen ? "accordion-item__expanded" : ""}`} onClick={()=> setIsOpen(is => !is)}>
+    <div className={`accordion-item ${isOpen ? "accordion-item__expanded" : ""}`} onClick={()=> setOpen(id)}>
       <div className="accordion-item__header"><p>{title}</p><ChevronDownIcon className="accordion-item__chevron"/></div>
       <div className="accordion-item__content">{content}</div>
     </div>
