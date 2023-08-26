f1c485c66cf13605d4149c31f630bc53927cab6d
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-08-26 20:00:29 +0330

    accordion chevron added and fully styled

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
-
+import {ChevronDownIcon,ChevronUpIcon} from "@heroicons/react/24/outline"
 function AccordionItem({ title, content }){
     const [isOpen,setIsOpen] = useState(false)
+    const styles = {
+        width:"1.2rem",
+        transition:"all 0.2s ease-out",
+        rotate:isOpen ? "180deg" : "0deg"
+    }
   return (
     <div className={`accordion-item ${isOpen ? "accordion-item__expanded" : ""}`} onClick={()=> setIsOpen(is => !is)}>
-      <div className="accordion-item__header">{title}</div>
+      <div className="accordion-item__header"><p>{title}</p><ChevronDownIcon style={styles}/></div>
       <div className="accordion-item__content">{content}</div>
     </div>
   );
